aa91874777aa36342ae65028005a2834609e5b31
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-08-31 15:35:05 -0500

    GET projects by Id endpoint created

--- api/projects/projectsRouter.js
+++ api/projects/projectsRouter.js
@@ -21,6 +21,23 @@ router.get('/', async (req, res) => {
 })
 
 
+// GET PROJECT BY ID
+router.get('/:id/project', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const project = await projectsDB.getProjectById(id);
+
+    if (project) {
+      res.json(project);
+    } else {
+      res.status(404).json({ message: 'Could not find project with given id.' })
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to get project' });
+  }
+});
+
 
 // GET RESOURCES
 router.get('/resources', async (req, res) => {
