29837460bcaaca3a3c0be67c7d73cfad04af305d
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2019-08-31 11:42:41 -0500

    database schema created

--- data/migrations/20190831113853_projects.js
+++ data/migrations/20190831113853_projects.js
@@ -1,8 +1,55 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+
+  // PROJECTS
+  .createTable('projects', table => {
+    table.increments()
+
+    table.string('name', 128).notNullable().unique()
+    table.text('description')
+    table.boolean('completed')
+
+  })
+
+   // RESOURCES
+  .createTable('resources', table => {
+    table.increments()
+
+    table.string('name', 128).notNullable().unique()
+    table.text('description')
+    
+    table.integer('project_id')
+    .unsigned()
+    .notNullable()
+    .references('id')
+    .inTable('projects')
+    .onUpdate('CASCADE')
+    .onDelete('CASCADE');
+  })
+
+
+   // TASKS
+  .createTable('tasks', table => {
+    table.increments()
+
+    table.text('description').notNullable()
+    table.string('notes', 128)
+    table.boolean('completed')
+
+    table.integer('project_id')
+    .unsigned()
+    .notNullable()
+    .references('id')
+    .inTable('projects')
+    .onUpdate('CASCADE')
+    .onDelete('CASCADE');
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+  .dropTableIfExists('tasks')
+  .dropTableIfExists('resources')
+  .dropTableIfExists('projects')
 };
